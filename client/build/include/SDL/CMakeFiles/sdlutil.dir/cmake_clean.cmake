df763432566c17fcb119b328a321d4796e56b08b
Nicolas Aguerre <naguerre@fi.uba.ar>
2020-06-06 17:01:01 -0300

    Agregados los assets para poder ejecutar prueba

---
+++ client/build/include/SDL/CMakeFiles/sdlutil.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sdlutil.dir/sdl_animated_sprite.cpp.o"
+  "CMakeFiles/sdlutil.dir/sdl_area.cpp.o"
+  "CMakeFiles/sdlutil.dir/sdl_error.cpp.o"
+  "CMakeFiles/sdlutil.dir/sdl_text.cpp.o"
+  "CMakeFiles/sdlutil.dir/sdl_texture.cpp.o"
+  "CMakeFiles/sdlutil.dir/sdl_texture_loader.cpp.o"
+  "CMakeFiles/sdlutil.dir/sdl_timer.cpp.o"
+  "CMakeFiles/sdlutil.dir/sdl_window.cpp.o"
+  "libsdlutil.a"
+  "libsdlutil.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sdlutil.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
