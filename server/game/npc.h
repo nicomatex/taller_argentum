0c2d9186aac5c99902344fd0384949c753d4850e
gdbien <fschischlo@fi.uba.ar>
2020-07-05 19:27:27 -0300

    agrego un par de cosas de npcs

--- server/game/npc.h
+++ server/game/npc.h
@@ -1,19 +1,25 @@
 #ifndef NPC_H
 #define NPC_H
 
+#include "../../include/nlohmann/json.hpp"
+#include "../../include/types.h"
 #include "entity.h"
-
-// TODO: implementar logica
+#include "map.h"
 
 class Npc : public Entity {
-   private:
-    unsigned int npc_id;
+  private:
+    Map& map;
+    uint32_t sprite_id;
 
    public:
-    Npc();
+    Npc(EntityId entity_id, nlohmann::json npc_info, Map& map);
     ~Npc();
+
     entity_type_t get_type() const override;
-    void update(uint64_t delta_t) override;  // aca ira lógica de pathfinding ?
+
+    nlohmann::json get_data() const override;
+
+    virtual void update(uint64_t delta_t) = 0;
 };
 
 #endif  // NPC_H
---
+++ server/game/npc_combat_component.h
@@ -0,0 +1,27 @@
+#ifndef NPC_COMBAT_COMPONENT_H
+#define NPC_COMBAT_COMPONENT_H
+
+#include <exception>
+#include "../../include/types.h"
+#include "combat_component.h"
+
+class CantAttackNpcException : public std::exception {
+   public:
+    const char *what() const throw();
+};
+
+class NpcCombatComponent : public CombatComponent {
+   public:
+    NpcCombatComponent();
+    ~NpcCombatComponent();
+
+    damage_t attack() override;
+    attack_result_t receive_damage(damage_t raw_damage) override;
+
+    void update(uint64_t) override;
+
+    /* Devuelve true si esta listo para atacar. */
+    bool attack_ready() const override;
+};
+
+#endif  // NPC_COMBAT_COMPONENT_H
