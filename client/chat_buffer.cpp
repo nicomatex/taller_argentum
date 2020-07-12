d4cd73b57642f51cedf60ecaaaa176a07bbf78e1
Nicolas Aguerre <naguerre@fi.uba.ar>
2020-07-12 16:25:41 -0300

    Ahora en el buffer de chat se parsean los saltos de linea para imprimir multiples lineas

--- client/chat_buffer.cpp
+++ client/chat_buffer.cpp
@@ -6,7 +6,15 @@ ChatBuffer::~ChatBuffer() {}
 
 void ChatBuffer::push(std::string message) {
     std::unique_lock<std::mutex> l(m);
-    buffer.push(message);
+    std::string::size_type pos = 0;
+    std::string::size_type prev = 0;
+
+    while((pos = message.find('\n',prev)) != std::string::npos){
+        buffer.push(message.substr(prev, pos - prev));
+        prev = pos + 1;
+    }
+
+    buffer.push(message.substr(prev));
 }
 
 void ChatBuffer::flush(Chat &chat) {
