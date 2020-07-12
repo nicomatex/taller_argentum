d404de3f031321dac56c92b9915caa524e7f647c
Nicolas Aguerre <naguerre@fi.uba.ar>
2020-07-12 18:35:32 -0300

    Agregada documentacion de VisualEffect

--- client/engine/visual_effect.h
+++ client/engine/visual_effect.h
@@ -2,6 +2,10 @@
 #define __VISUAL_EFFECT_H
 #include "renderizable_object.h"
 
+/**
+ * @brief Clase para mostrar efectos visuales en posiciones dinamicas.
+ * 
+ */
 class VisualEffect: public RenderizableObject{
     private:
         SDLSprite sprite;
@@ -9,6 +13,11 @@ class VisualEffect: public RenderizableObject{
     public:
         VisualEffect(SDLSprite &sprite, visual_info_t visual_info);
 
+        /**
+         * @brief Renderiza el efecto en pantalla.
+         * 
+         * @param dest Rectangulo de la pantalla donde se va a renderizar.
+         */
         void render(SDL_Rect dest) override;
 };
 
