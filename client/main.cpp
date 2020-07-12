ea5812944e505306c4f1e737ad61f88e8d2122be
Nicolas Aguerre <naguerre@fi.uba.ar>
2020-07-12 04:59:28 -0300

    Agregado script para crear paquete de debian. Agregado soporte en el cliente para que busque los archivos en /usr/local/share/argentum los assets si no los encuentra en directorio local

--- client/main.cpp
+++ client/main.cpp
@@ -13,6 +13,15 @@ int main(void) {
     json config;
     try {
         std::ifstream config_file(CLIENT_CONFIG_FILE);
+        if (!config_file.is_open()) {
+            if (chdir("/usr/local/share/argentum") < 0) {
+                std::cerr
+                    << "Config file not found. Is the game installed correctly?"
+                    << std::endl;
+                return -1;
+            }
+            config_file.open(CLIENT_CONFIG_FILE);
+        }
         config = json::parse(config_file);
     } catch (std::exception &e) {
         std::cerr << MSG_ERR_CONFIG_FILE << std::endl;
